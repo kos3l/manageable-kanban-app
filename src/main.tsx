f37642582c125a1275354f510346f9e17c23d139
kos3l <kos3l@wp.pl>
2023-03-07 12:11:36 +0100

    added react query and react router set up

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import Root from "./routes/root/root";
+import ErrorPage from "./routes/root/error-page";
+
 import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
