ffb093d8a97c5053c6671693b9c0a9fd69e432d9
kos3l <kos3l@wp.pl>
2023-04-27 10:52:56 +0200

    made login and register responsive and added gradients

--- src/routes/login/login.tsx
+++ src/routes/login/login.tsx
@@ -13,6 +13,7 @@ import ActionButton from "../../ui/buttons/ActionButton";
 import TextInput from "../../ui/inputs/TextInput";
 import { useNavigate } from "react-router-dom";
 import useAuthService from "../../hooks/service/useAuthService";
+import gradient from "../../assets/gradient.svg";
 
 export default function Login() {
   const [email, setEmail] = useState<string>("");
@@ -36,62 +37,69 @@ export default function Login() {
   });
 
   return (
-    <div className="flex grow">
-      <div className="flex w-full items-center justify-center">
-        <div className="flex h-max w-1/3 flex-col gap-6 pb-12">
-          <div className="flex h-max w-full flex-col rounded-xl border-2 border-neutral-600 bg-neutral-800/50 p-4">
-            <h1 className="m-0 mb-1 font-serif text-2xl font-bold tracking-wider">
-              Welcome Back!
-            </h1>
-            <p className="m-0 font-sans text-sm text-neutral-500">
-              Enter your email and password to sign into the system and start
-              working!
-            </p>
-            <div className="mt-4 flex h-max w-full flex-wrap gap-4">
-              <TextInput
-                icon={<AtSymbolIcon className="m-0 w-5 p-0"></AtSymbolIcon>}
-                placeholder="Email.."
-                value={email}
-                onChange={(val) => setEmail(val)}
-              ></TextInput>
-              <TextInput
-                icon={<KeyIcon className="m-0 w-4 p-0"></KeyIcon>}
-                placeholder="Password.."
-                value={password}
-                onChange={(val) => setPassword(val)}
-              ></TextInput>
+    <>
+      <div className="absolute bottom-0 left-0 z-10 flex w-full">
+        <img src={gradient} alt="" className="w-full opacity-50" />
+      </div>
+      <div className="relative z-20 flex grow">
+        <div className="flex w-full items-center justify-center p-4">
+          <div className="flex h-max w-full flex-col  gap-6 pb-12 sm:w-2/3 md:w-[35rem] 2xl:w-2/3">
+            <div className="flex h-max w-full flex-col rounded-xl border-2 border-neutral-600 bg-neutral-800/50 p-4">
+              <h1 className="m-0 mb-1 font-serif text-2xl font-bold tracking-wider">
+                Welcome Back!
+              </h1>
+              <p className="m-0 font-sans text-sm text-neutral-500">
+                Enter your email and password to sign into the system and start
+                working!
+              </p>
+              <div className="mt-4 flex h-max w-full flex-wrap gap-4">
+                <TextInput
+                  icon={<AtSymbolIcon className="m-0 w-5 p-0"></AtSymbolIcon>}
+                  placeholder="Email.."
+                  value={email}
+                  onChange={(val) => setEmail(val)}
+                ></TextInput>
+                <TextInput
+                  icon={<KeyIcon className="m-0 w-4 p-0"></KeyIcon>}
+                  placeholder="Password.."
+                  value={password}
+                  onChange={(val) => setPassword(val)}
+                ></TextInput>
+              </div>
             </div>
-          </div>
-          <div className="w-full">
-            <ActionButton
-              content={"SIGN IN"}
-              onClick={() => {
-                mutation.mutate({
-                  email: email,
-                  password: password,
-                });
-              }}
-              icon={
-                <ArrowLeftOnRectangleIcon className="w-6 text-indigo-600"></ArrowLeftOnRectangleIcon>
-              }
-            ></ActionButton>
-          </div>
-          <div className="flex w-full justify-center">
-            <p className="text-neutral-500">
-              Don't have an account yet?
-              <Link to={"../register"}>
-                <span className="ml-1 text-neutral-200/80 underline">
-                  Register
-                </span>
-              </Link>
-              <Link to={"/user/test"}>
-                <span className="ml-1 text-neutral-200/80 underline">test</span>
-              </Link>
-            </p>
+            <div className="w-full">
+              <ActionButton
+                content={"SIGN IN"}
+                onClick={() => {
+                  mutation.mutate({
+                    email: email,
+                    password: password,
+                  });
+                }}
+                icon={
+                  <ArrowLeftOnRectangleIcon className="w-6 text-indigo-600"></ArrowLeftOnRectangleIcon>
+                }
+              ></ActionButton>
+            </div>
+            <div className="flex w-full justify-center">
+              <p className="text-neutral-500">
+                Don't have an account yet?
+                <Link to={"../register"}>
+                  <span className="ml-1 text-neutral-200/80 underline">
+                    Register
+                  </span>
+                </Link>
+                <Link to={"/user/test"}>
+                  <span className="ml-1 text-neutral-200/80 underline">
+                    test
+                  </span>
+                </Link>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 function log(newUser: ICreateLoginDTO): Promise<unknown> {
--- src/routes/register/Register.tsx
+++ src/routes/register/Register.tsx
@@ -5,7 +5,6 @@ import {
   KeyIcon,
   UserPlusIcon,
 } from "@heroicons/react/24/solid";
-import axios from "axios";
 import { useState } from "react";
 import { useMutation, useQuery } from "react-query";
 import { Link } from "react-router-dom";
@@ -15,6 +14,7 @@ import ActionButton from "../../ui/buttons/ActionButton";
 import DateInput from "../../ui/inputs/DateInput";
 import TextInput from "../../ui/inputs/TextInput";
 import { DateHelper } from "../../util/helpers/DateHelper";
+import gradient from "../../assets/gradient.svg";
 
 export default function Register() {
   const [firstName, setFirstName] = useState<string>("");
@@ -37,81 +37,86 @@ export default function Register() {
   });
 
   return (
-    <div className="flex grow">
-      <div className="flex w-full items-center justify-center">
-        <div className="flex h-max w-1/3 flex-col gap-6 pb-12">
-          <div className="flex h-max w-full flex-col rounded-xl border-2 border-neutral-600 bg-neutral-800/50 p-4">
-            <h1 className="m-0 mb-1 font-serif text-2xl font-bold tracking-wider">
-              Let's start!
-            </h1>
-            <p className="m-0 font-sans text-sm text-neutral-500">
-              Create a new account for free.
-            </p>
-            <div className="mt-4 flex h-max w-full flex-wrap gap-4">
-              <TextInput
+    <>
+      <div className="absolute bottom-0 left-0 z-10 flex w-full">
+        <img src={gradient} alt="" className="w-full opacity-50" />
+      </div>
+      <div className="relative z-20 flex grow">
+        <div className="flex w-full items-center justify-center p-4">
+          <div className="flex h-max w-full flex-col gap-6 pb-12 sm:w-2/3 md:w-[35rem] 2xl:w-2/3">
+            <div className="flex h-max w-full flex-col rounded-xl border-2 border-neutral-600 bg-neutral-800/50 p-4">
+              <h1 className="m-0 mb-1 font-serif text-2xl font-bold tracking-wider">
+                Let's start!
+              </h1>
+              <p className="m-0 font-sans text-sm text-neutral-500">
+                Create a new account for free.
+              </p>
+              <div className="mt-4 flex h-max w-full flex-wrap gap-4">
+                <TextInput
+                  icon={
+                    <IdentificationIcon className="m-0 w-5 p-0"></IdentificationIcon>
+                  }
+                  placeholder="First Name.."
+                  value={firstName}
+                  onChange={(val) => setFirstName(val)}
+                ></TextInput>
+                <TextInput
+                  icon={
+                    <IdentificationIcon className="m-0 w-5 p-0"></IdentificationIcon>
+                  }
+                  placeholder="Last Name.."
+                  value={lastName}
+                  onChange={(val) => setLastName(val)}
+                ></TextInput>
+                <DateInput
+                  icon={<CakeIcon className="m-0 w-5 p-0"></CakeIcon>}
+                  value={DateHelper.formatDateToString(birthday, "YYYY-MM-DD")}
+                  onChange={(val) => setBirthday(new Date(val))}
+                ></DateInput>
+                <TextInput
+                  icon={<AtSymbolIcon className="m-0 w-5 p-0"></AtSymbolIcon>}
+                  placeholder="Email.."
+                  value={email}
+                  onChange={(val) => setEmail(val)}
+                ></TextInput>
+                <TextInput
+                  icon={<KeyIcon className="m-0 w-4 p-0"></KeyIcon>}
+                  placeholder="Password.."
+                  value={password}
+                  onChange={(val) => setPassword(val)}
+                ></TextInput>
+              </div>
+            </div>
+            <div className="w-full">
+              <ActionButton
+                onClick={() => {
+                  mutation.mutate({
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email,
+                    password: password,
+                    birthdate: birthday,
+                  });
+                }}
+                content={"SIGN UP"}
                 icon={
-                  <IdentificationIcon className="m-0 w-5 p-0"></IdentificationIcon>
+                  <UserPlusIcon className="w-5 text-indigo-600"></UserPlusIcon>
                 }
-                placeholder="First Name.."
-                value={firstName}
-                onChange={(val) => setFirstName(val)}
-              ></TextInput>
-              <TextInput
-                icon={
-                  <IdentificationIcon className="m-0 w-5 p-0"></IdentificationIcon>
-                }
-                placeholder="Last Name.."
-                value={lastName}
-                onChange={(val) => setLastName(val)}
-              ></TextInput>
-              <DateInput
-                icon={<CakeIcon className="m-0 w-5 p-0"></CakeIcon>}
-                value={DateHelper.formatDateToString(birthday, "YYYY-MM-DD")}
-                onChange={(val) => setBirthday(new Date(val))}
-              ></DateInput>
-              <TextInput
-                icon={<AtSymbolIcon className="m-0 w-5 p-0"></AtSymbolIcon>}
-                placeholder="Email.."
-                value={email}
-                onChange={(val) => setEmail(val)}
-              ></TextInput>
-              <TextInput
-                icon={<KeyIcon className="m-0 w-4 p-0"></KeyIcon>}
-                placeholder="Password.."
-                value={password}
-                onChange={(val) => setPassword(val)}
-              ></TextInput>
+              ></ActionButton>
+            </div>
+            <div className="flex w-full justify-center">
+              <p className="text-neutral-500">
+                Already have an account?
+                <Link to={"../login"}>
+                  <span className="ml-1 text-neutral-200/80 underline">
+                    Login
+                  </span>
+                </Link>
+              </p>
             </div>
           </div>
-          <div className="w-full">
-            <ActionButton
-              onClick={() => {
-                mutation.mutate({
-                  firstName: firstName,
-                  lastName: lastName,
-                  email: email,
-                  password: password,
-                  birthdate: birthday,
-                });
-              }}
-              content={"SIGN UP"}
-              icon={
-                <UserPlusIcon className="w-5 text-indigo-600"></UserPlusIcon>
-              }
-            ></ActionButton>
-          </div>
-          <div className="flex w-full justify-center">
-            <p className="text-neutral-500">
-              Already have an account?
-              <Link to={"../login"}>
-                <span className="ml-1 text-neutral-200/80 underline">
-                  Login
-                </span>
-              </Link>
-            </p>
-          </div>
         </div>
-      </div>
-    </div>
+      </div>{" "}
+    </>
   );
 }
